ff85ad516e1355e5680779e2dfb24ce890e32952
shruthinswamy <shruthi.swamy@gmail.com>
2023-06-14 20:30:23 +0530

    commit into new branch

--- src/test/java/demo/Demo.java
+++ src/test/java/demo/Demo.java
@@ -4,10 +4,13 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Demo file");
+		System.out.println("Demo file"); 
 		
 		System.out.println("Adding new statement to understand GIT conflict");
 		//trying to push without pulling latest version.
+		
+		System.out.println("create a new branch and then commit");
+		//branch name taken as TC_001
 	}
 
 }
